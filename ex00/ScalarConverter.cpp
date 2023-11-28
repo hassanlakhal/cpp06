0172ca47b62b554cb4c4fda264243d2d016036eb
Hassan Lakhal <hlakhal-@e1r3p11.1337.ma>
2023-11-28 08:56:01 +0100

    fixed ex00

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: hlakhal- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 00:07:35 by hlakhal-          #+#    #+#             */
-/*   Updated: 2023/11/27 11:07:38 by hlakhal-         ###   ########.fr       */
+/*   Updated: 2023/11/27 12:09:15 by hlakhal-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,6 @@ ScalarConverter::~ScalarConverter()
     
 }
 
-
-
 char ScalarConverter::getChar(const std::string& str)
 {
    
@@ -35,7 +33,12 @@ char ScalarConverter::getChar(const std::string& str)
     else if (my_str >> intValue && intValue <= 127 && isprint(static_cast<char>(intValue)))
         c = intValue;
     else
-        std::cout << "char: Non displayable" << std::endl;
+    {
+        if(str != "nan")
+            throw std::runtime_error("char: Non displayable");
+        else if (str == "nan")
+             throw std::runtime_error("char: impossible");
+    }
     return c;
 }
 
@@ -46,26 +49,61 @@ int ScalarConverter::getInt(const std::string& str)
     if (str.size() == 1 && isalpha(str[0]))
         return str[0];
     else if (!(my_str >> intValue))
-        return 0;
+        throw std::runtime_error("impossible");
     return intValue;
 }
 
 double ScalarConverter::getDouble(const std::string& str)
 {
+    if (str.size() == 1 && isalpha(str[0]))
+        return str[0];
     double floatValue = std::stod(str.c_str());
     return floatValue;
 }
 
 float ScalarConverter::getFloat(const std::string& str)
 {
+    if (str.size() == 1 && isalpha(str[0]))
+        return str[0];
     float floatValue = std::stof(str.c_str());
     return floatValue;
 }
 
 void ScalarConverter::convert(const std::string& str)
 {
-    std::cout << ScalarConverter::getChar(str) <<  std::endl;
-    std::cout << ScalarConverter::getInt(str) <<  std::endl;
-    std::cout << std::fixed << std::setprecision(1) << ScalarConverter::getDouble(str) <<  std::endl;
-    std::cout << std::fixed << std::setprecision(1) << ScalarConverter::getFloat(str) << "f" <<  std::endl;
+    try
+    {    
+        char c = ScalarConverter::getChar(str);
+        std::cout << "Char : "<< "'" <<  c << "'" <<  std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        std::cout << "Int : "<< ScalarConverter::getInt(str) <<  std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        float f = ScalarConverter::getFloat(str);
+        std::cout << "float : "<<std::fixed << std::setprecision(1) << f << "f" <<  std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        double d = ScalarConverter::getDouble(str);
+        std::cout << "double : " << std::fixed << std::setprecision(1) << d <<  std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 }
